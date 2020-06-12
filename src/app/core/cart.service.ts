52548707de17ea4fe184b2682583d868277d8a2a
Costath <thalesdaniel@gmail.com>
2020-06-12 15:58:26 -0400

    added localStorage functionality

--- src/app/core/cart.service.ts
+++ src/app/core/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { ICartProduct } from '../shared/interfaces';
+import { ICartItem } from '../shared/interfaces';
 import { IProduct } from '../shared/interfaces';
 import { DataService } from '../core/data.service';
 import { RouteReuseStrategy } from '@angular/router';
@@ -9,24 +9,68 @@ import { RouteReuseStrategy } from '@angular/router';
   providedIn: 'root'
 })
 export class CartService {
-  cartProducts: ICartProduct[] = [];
   constructor(private dataService: DataService) { }
 
   addItem(product: IProduct){
-    const cartItem = this.cartProducts.filter(p => p.productId === product.productId);
-
-    if (cartItem.length > 0) {
-      cartItem[0].quantity++;
-    }else {
-      const cartProduct: ICartProduct = {
+    if (localStorage.getItem('cart') == null) {
+      const firstCartProduct: ICartItem = {
         productId: product.productId,
         quantity: 1
       };
-      this.cartProducts.push(cartProduct);
+      const cart: any[] = [];
+      cart.push(JSON.stringify(firstCartProduct));
+      localStorage.setItem('cart', JSON.stringify(cart));
+    }else{
+      const cart = JSON.parse(localStorage.getItem('cart'));
+      let itemIndex = -1;
+
+      for (let i = 0; i < cart.length; i++) {
+        const existingProduct: ICartItem = JSON.parse(cart[i]);
+        if (existingProduct.productId === product.productId) {
+          itemIndex = i;
+          break;
+        }
+      }
+
+      if (itemIndex === -1) {
+        const newCartProduct: ICartItem = {
+          productId: product.productId,
+          quantity: 1
+        };
+        cart.push(JSON.stringify(newCartProduct));
+      }else {
+        const existingProduct = JSON.parse(cart[itemIndex]);
+        existingProduct.quantity ++;
+        cart[itemIndex] = JSON.stringify(existingProduct);
+      }
+
+      localStorage.setItem('cart', JSON.stringify(cart));
     }
   }
 
+  removeItem(product: IProduct){
+    const cart: any[] = JSON.parse(localStorage.getItem('cart'));
+    let cartItem: ICartItem;
+    let itemIndex;
+    cart.forEach((itemRaw, index) => {
+      const item: ICartItem = JSON.parse(itemRaw);
+      if (item.productId === product.productId) {
+        cartItem = item;
+        itemIndex = index;
+      }
+    });
+
+    if (cartItem.quantity > 1) {
+      cartItem.quantity--;
+      cart.splice(itemIndex, 1, JSON.stringify(cartItem));
+    }else {
+      cart.splice(itemIndex, 1);
+    }
+
+    (cart.length > 0) ? localStorage.setItem('cart', JSON.stringify(cart)) : localStorage.removeItem('cart');
+  }
+
   getCartProducts() {
-    return this.cartProducts;
+    return JSON.parse(localStorage.getItem('cart'));
   }
 }
