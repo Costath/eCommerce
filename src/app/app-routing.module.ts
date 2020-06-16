87fe5467ae33884dc1b6982010cf787854090f13
Costath <thalesdaniel@gmail.com>
2020-06-16 00:20:40 -0400

    Renamed path parameter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { pathToFileURL } from 'url';
 import { ContentComponent } from './content/content.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
@@ -10,7 +9,7 @@ import { CartPageComponent } from './cart/cart-page/cart-page.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: LandingPageComponent}, // change ContentComponent to the home page when its done
-  { path: 'products/:id', component: ContentComponent },
+  { path: 'products/:cat', component: ContentComponent },
   { path: 'products', component: ContentComponent },
   { path: 'contact', component: ContactPageComponent },
   { path: 'about', component: AboutPageComponent },
